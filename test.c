0d6716b76640dc5fe11f489630e00b5a62a7135c
bwhitmire <bcwhitmire@gmail.com>
2025-01-15 14:51:15 -0500

    figured out the atof

--- test.c
+++ test.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
     float temperature;
+
     if (argc > 1)
     {
-        int i;
-        for (i = 0; i < argc; i++)
-        {
-            printf("%s\n", argv[i]);
-        }
+        temperature = atof(argv[1]);
     }
-    // printf("Please provide a temperature in Ferinhieight.\n");
-    // scanf("%f", &temperature);
-    // printf("%d\n", temperature > 70 && temperature < 85);
+    else
+    {
+        printf("Please provide a temperature in Ferinhieight.\n");
+        scanf("%f", &temperature);
+    }
+
+    printf("%d\n", temperature > 70 && temperature < 85);
+
     return 0;
+}
