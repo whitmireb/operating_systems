ac6d27550909398bf750f6f242e904e15f8a18e4
bwhitmire <bcwhitmire@gmail.com>
2025-01-15 14:18:25 -0500

    finished exercise 1

--- exercise1.c
+++ exercise1.c
@@ -2,6 +2,9 @@
 
 int main()
 {
-
+    float temperature;
+    printf("Please provide a temperature in Ferinhieight.\n");
+    scanf("%f", &temperature);
+    printf("%d\n", temperature > 70 && temperature < 85);
     return 0;
 }
